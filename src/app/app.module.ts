978623b041f671fcf66eaa629e0301082505cacc
unknown <rashmihebbar23@gmail.com>
2019-09-18 00:00:33 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import {MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule} from '@angular/material';
 
+// const routes = [
+
+//   { path: '/', component: LoginComponent, pathMtch: 'full' },
+
+// ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
