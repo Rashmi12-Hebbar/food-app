1f4d09ac983c9d6128b64c317c155360e1b26d95
unknown <rashmihebbar23@gmail.com>
2019-09-18 21:39:19 +0530

    ra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RestuarantComponent } from './restuarant/restuarant.component';
 
 const routes: Routes = [
-    {
-        path:'',
-        component:LoginComponent,
-        pathMatch:"full"
-    }
+   
+  {path:'login',component:LoginComponent,pathMatch:"full"},
+  { path:'register',component:RegisterComponent},
+  { path : 'restuarant', component:RestuarantComponent}
 ];
 
 @NgModule({
